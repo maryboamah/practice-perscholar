87660cff4af0ff4e73c0beb8a4b70155897dcabd
maryboamah <maryboamah680@gmail.com>
2022-03-19 04:39:48 -0400

    adding home page

---
+++ portfolio-website/app.js
@@ -0,0 +1,42 @@
+const sections = document.querySelectorAll('.section');
+const secBtns = document.querySelectorAll('.controls');
+const secBtn = document.querySelectorAll('.control');
+const allSections = document.querySelector('.main-content');
+
+function PageTransitions(){
+    //button click active class
+    for(let i =0; i<secBtn.length; i++){
+        secBtn[i].addEventListener('click', ()=> {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+            //or this.className +=' active-btn' if not using arrow function this represent secBtn
+           secBtn[i].className +=' active-btn'
+
+        }) 
+        
+    }
+
+    //sections active class
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            //remove selected from othjers
+            secBtns.forEach((btn)=>{
+               
+                btn.classList.remove('active')
+                
+
+
+            })
+            e.target.classList.add('active')
+            // hide other sections
+            sections.forEach((section)=>{
+                section.classList.remove("active")
+            })
+            const element = document.getElementById(id)
+            element.classList.add('active')
+        }
+    })
+}
+
+PageTransitions();
