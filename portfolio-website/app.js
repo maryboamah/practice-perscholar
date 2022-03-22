6062fa16e89a711f0842863c384dab6f29eba6b1
maryboamah <maryboamah680@gmail.com>
2022-03-22 15:06:33 -0400

    adding responsiveness

--- portfolio-website/app.js
+++ portfolio-website/app.js
@@ -37,6 +37,13 @@ function PageTransitions(){
             element.classList.add('active')
         }
     })
+
+    //toggle theme
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', ()=>{
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
 }
 
 PageTransitions();
